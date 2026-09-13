277675b08165273e760b15984b070d96a26af2e3
Nicolas Sousa <niolaspaixao34@gmail.com>
2026-09-13 20:44:18 -0300

    feat: Reorganiza e melhora estrutura do projeto

--- my-app/src/main.tsx
+++ my-app/src/main.tsx
@@ -1,36 +1,39 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import './index.css';
+import './global.css';
 
 import App from './App.tsx';
-import Home from './pages/Home.tsx';
-import Sobre from './pages/Sobre.tsx';
-import Faq from './pages/Faq.tsx';
-import Contato from './pages/Contato.tsx';
-import Integrantes from './pages/Integrantes.tsx';
-import Simulador from './pages/Simulador.tsx';
-import Dashboard from './pages/Dashboard.tsx';
-import Recompensas from './pages/Recompensas.tsx';
+import Home from './routes/Home/index.tsx';
+import Sobre from './routes/Sobre/index.tsx';
+import Faq from './routes/Faq/index.tsx';
+import Contato from './routes/Contato/index.tsx';
+import Integrantes from './routes/Integrantes/index.tsx';
+import Simulador from './routes/Simulador/index.tsx';
+import Dashboard from './routes/Dashboard/index.tsx';
+import Recompensas from './routes/Recompensas/index.tsx';
+import RecompensaDetalhes from './routes/RecompensaDetalhes/index.tsx';
 
 const router = createBrowserRouter([
-{
-path: '/', 
-element: <App />, 
-children: [
-{path: '/', element: <Home />},
-{path: '/sobre', element: <Sobre />},
-{path: '/faq', element: <Faq />},
-{path: '/contato', element: <Contato />},
-{path: '/integrantes', element: <Integrantes />},
-{path: '/simulador', element: <Simulador />},
-{path: '/dashboard', element: <Dashboard />},
-{path: '/recompensas', element: <Recompensas />}
-]
-}
+  {
+    path: '/', 
+    element: <App />, 
+    children: [
+      {path: '/', element: <Home />},
+      {path: '/sobre', element: <Sobre />},
+      {path: '/faq', element: <Faq />},
+      {path: '/contato', element: <Contato />},
+      {path: '/integrantes', element: <Integrantes />},
+      {path: '/simulador', element: <Simulador />},
+      {path: '/dashboard', element: <Dashboard />},
+      {path: '/recompensas', element: <Recompensas />},
+      {path: '/recompensas/:id', element: <RecompensaDetalhes />}
+    ]
+  }
 ]);
 
 createRoot(document.getElementById('root')!).render(
-<StrictMode>
-<RouterProvider router={router} />
-</StrictMode>,
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+);
